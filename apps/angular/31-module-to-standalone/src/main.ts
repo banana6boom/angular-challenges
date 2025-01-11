d82e801daa31386f9bf31385a3ae9932e0e99174
Oleg Shchegolev <themadhouse@mail.ru>
2025-01-11 23:51:20 +0300

    feat: module to standalone challenge solution

--- apps/angular/31-module-to-standalone/src/main.ts
+++ apps/angular/31-module-to-standalone/src/main.ts
@@ -1,6 +1,8 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
+import { appRoutes } from '@angular-challenges/module-to-standalone/shell';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { AppComponent } from './app/app.component';
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(appRoutes)],
+}).catch((err) => console.error(err));
--- libs/module-to-standalone/shell/src/lib/main-shell.routes.ts
+++ libs/module-to-standalone/shell/src/lib/main-shell.routes.ts
@@ -1,36 +1,41 @@
 import { IsAuthorizedGuard } from '@angular-challenges/module-to-standalone/admin/shared';
+import { provideToken } from '@angular-challenges/module-to-standalone/core/providers';
 import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
   {
     path: 'home',
-    loadChildren: () =>
+    loadComponent: () =>
       import('@angular-challenges/module-to-standalone/home').then(
-        (m) => m.ModuleToStandaloneHomeModule,
+        (m) => m.HomeComponent,
+      ),
+    providers: [provideToken('main-shell-token')],
+  },
+  {
+    path: 'admin',
+    canMatch: [IsAuthorizedGuard],
+    loadChildren: () =>
+      import('@angular-challenges/module-to-standalone/admin/feature').then(
+        (m) => m.adminFeatureRoutes,
       ),
   },
   {
     path: 'admin',
-    canActivate: [IsAuthorizedGuard],
-    loadChildren: () =>
-      import('@angular-challenges/module-to-standalone/admin/feature').then(
-        (m) => m.AdminFeatureModule,
+    loadComponent: () =>
+      import('@angular-challenges/module-to-standalone/forbidden').then(
+        (m) => m.ForbiddenComponent,
       ),
   },
   {
     path: 'user',
     loadChildren: () =>
       import('@angular-challenges/module-to-standalone/user/shell').then(
-        (m) => m.UserShellModule,
-      ),
-  },
-
-  {
-    path: 'forbidden',
-    loadChildren: () =>
-      import('@angular-challenges/module-to-standalone/forbidden').then(
-        (m) => m.ForbiddenModule,
+        (m) => m.userShellRoutes,
       ),
   },
 ];
